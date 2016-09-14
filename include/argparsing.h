519f9644621e29c0e0826ca55c1164d12e68767c
sauercrowd <jonadev95@posteo.org>
2016-09-14 20:28:32 +0200

    fixed stuff

--- include/argparsing.h
+++ include/argparsing.h
@@ -3,22 +3,6 @@
 
 #include <argp.h>
 
-const char *argp_program_version = 
-	"ext2-inspect 0.0.1";
-const char * argp_program_bug_address = 
-	"jonadev95@posteo.org";
-
-static char doc[] = 
-	"ext2-inspect -- a program for inspecting ext2 filesystems on the fly.";
-
-static char args_doc[] = "COMMAND PATH";
-
-static struct argp_option options[] = {
-	{"verbose", 'v', 0, 0, "Produce verbose output"},
-	{0}
-};
-
-
 struct arguments
 {
 	char *args[2];
@@ -28,5 +12,4 @@ typedef struct arguments arguments;
 
 int parse(arguments* args, int argc, char** argv);
 
-static struct argp argp = { options, parse_opt, args_doc, doc };
 #endif
--- src/argparsing.c
+++ src/argparsing.c
@@ -1,24 +1,45 @@
 #include "argparsing.h"
+#include <argp.h>
+
+const char *argp_program_version = 
+	"ext2-inspect 0.0.1";
+const char * argp_program_bug_address = 
+	"jonadev95@posteo.org";
+
+static char doc[] = 
+	"ext2-inspect -- a program for inspecting ext2 filesystems on the fly.";
+
+static char args_doc[] = "COMMAND PATH";
+
+static struct argp_option options[] = {
+	{"verbose", 'v', 0, 0, "Produce verbose output"},
+	{0}
+};
+
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state){
 	struct arguments *arguments = state->input;
 	switch(key){
 		case 'v':
-			arguments->verbose = 1;
-			break;
+		        arguments->verbose = 1;
+	                break;
 
-		case ARGP_KEY_ARG:
-			if(state->arg_num >= 2)
-				argp_usage(state);
-			arguments->args[state->arg_num] = arg;
-			break;
-		default:
-			return ARGP_ERR_UNKNOWN;
-	}
-	return 0;
+	        case ARGP_KEY_ARG:
+		        if(state->arg_num >= 2)
+		                argp_usage(state);
+	                arguments->args[state->arg_num] = arg;
+	                break;
+                default:
+                        return ARGP_ERR_UNKNOWN;
+        }
+        return 0;
 }
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int parse(arguments* args, int argc, char** argv){
-	argp_parse(&argp, argc, argv, 0, 0, args);
-	return 0;
+        argp_parse(&argp, argc, argv, 0, 0, args);
+        return 0;
 }
+
+
+
